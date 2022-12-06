6fcedc198ea0aae7018bdecc7fd78e2169782b35
Weijian Yang <weijian.yang@rea-group.com>
2022-12-06 19:09:51 +0800

    call local environment successfully.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Post('/mutate')
+  @HttpCode(200)
+  getHello(@Body() body: Record<any, string>): string {
+    console.log(JSON.stringify(body));
     return this.appService.getHello();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const ROOT_DIR = path.dirname(__dirname);
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: fs.readFileSync(`${ROOT_DIR}/ca/server.key`),
+      cert: fs.readFileSync(`${ROOT_DIR}/ca/server.crt`),
+    },
+  });
   await app.listen(3000);
 }
+
 bootstrap();
