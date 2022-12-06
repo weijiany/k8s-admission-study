78499800a352939d68f382fca0bfa259fabd34e3
Weijian Yang <weijian.yang@rea-group.com>
2022-12-06 20:14:21 +0800

    implement the mutating controller.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,54 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Post } from '@nestjs/common';
+
+const NGINX_PROXY_KEY = 'k8s-admission-study/proxy';
+
+const base64 = (o: Record<any, any>): string =>
+  Buffer.from(JSON.stringify(o)).toString('base64');
+
+const nginxProxyPatchObj = (op: string, index: number): Record<any, any> => ({
+  op: op,
+  path: `/spec/template/spec/containers/${index}`,
+  value: {
+    name: 'nginx-proxy',
+    image: 'nginx:1.23.2',
+    ports: [{ containerPort: 80 }],
+  },
+});
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Post('/mutate')
-  @HttpCode(200)
-  getHello(@Body() body: Record<any, string>): string {
-    console.log(JSON.stringify(body));
-    return this.appService.getHello();
+  getHello(@Body() body: Record<any, string>): Record<any, any> {
+    const uid = body['request']['uid'];
+    const proxyIndex = this.proxyIndex(body);
+    const enableProxy = Boolean(
+      body['request']['object']['metadata']['annotations'][NGINX_PROXY_KEY],
+    );
+
+    const op = [];
+    if (enableProxy) {
+      op.push(nginxProxyPatchObj('replace', proxyIndex));
+    } else if (proxyIndex != -1) {
+      op.push(nginxProxyPatchObj('remove', proxyIndex));
+    }
+
+    return {
+      apiVersion: 'admission.k8s.io/v1',
+      kind: 'AdmissionReview',
+      response: {
+        uid,
+        allowed: true,
+        patchType: 'JSONPatch',
+        patch: base64(op),
+      },
+    };
+  }
+
+  proxyIndex(body: Record<any, any>): number {
+    const containers =
+      body['request']['object']['spec']['template']['spec']['containers'];
+    return containers
+      .map((container) => container['name'])
+      .indexOf('nginx-proxy');
   }
 }
